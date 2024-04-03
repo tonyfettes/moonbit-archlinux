d687ee3e84bb01e59f15d70cab82eac8442df6f3
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-03 21:31:05 +0800

    feat(ci): add release and test workflows

--- .github/actions/bundle/entrypoint.sh
+++ .github/actions/bundle/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 (cd /usr/share/moonbit/lib/core && moon bundle)
+(cd /root && moon new test && cd test && moon check)
 bsdtar -C /usr/share/moonbit/lib/ -a -cf /github/workspace/core.zip core
