d4fd71deceeb0bafba1c088162b8e84d6265f122
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-03 22:03:47 +0800

    fix(ci): permission denied on entrypoint.sh

--- .github/actions/package/entrypoint.sh
+++ .github/actions/package/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -xeuo
+
 cp -r /github/workspace/* /build/
-makepkg
-sudo cp ./*.pkg.tar.zst /github/workspace
+(cd /build && makepkg)
+sudo cp /build/*.pkg.tar.zst /github/workspace
