8ad932833f4f1ad11081c91a2bb2c45291185549
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-03 21:37:24 +0800

    fix(ci): test workflow

--- .github/actions/bundle/entrypoint.sh
+++ .github/actions/bundle/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-(cd /usr/share/moonbit/lib/core && moon bundle)
-(cd /root && moon new test && cd test && moon check)
+(cd /usr/share/moonbit/lib/core && /usr/lib/moon bundle)
+
+(cd /root && /usr/lib/moon new test && cd test && sh /github/workspace/moon.sh check)
+
 bsdtar -C /usr/share/moonbit/lib/ -a -cf /github/workspace/core.zip core
